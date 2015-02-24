f8a157bae9ce734634f1b2d5ba986a3ccd413802
Mario Pareja <pareja.mario@gmail.com>
2015-02-24 16:50:06 -0500

    refactor: move initialization of `connect` to beforeEach.

--- test/amqplib.js
+++ test/amqplib.js
@@ -21,8 +21,14 @@ function getStubAmqplib() {
 }
 
 describe('amqplib support', function () {
+  var stub, connect;
+  beforeEach(function (done) {
+    stub = getStubAmqplib();
+    connect = amqplibConnect(stub);
+    done();
+  });
+
   it('can connect', function (done) {
-    var connect = amqplibConnect(getStubAmqplib());
     connect('amqp://guest:guest@localhost:5672', function (err, channel) {
       assert.isDefined(channel);
       assert.isDefined(channel.bind);
@@ -31,10 +37,8 @@ describe('amqplib support', function () {
   });
 
   it('handles errors connecting', function (done) {
-    var stub = getStubAmqplib();
     stub.connect = sinon.stub().yieldsAsync(new Error('connect'));
 
-    var connect = amqplibConnect(stub);
     connect('amqp://guest:guest@localhost:5672', function (err) {
       assert.instanceOf(err, Error);
       done();
@@ -42,10 +46,8 @@ describe('amqplib support', function () {
   });
 
   it('handles errors creating channel', function (done) {
-    var stub = getStubAmqplib();
     stub.connection.createChannel = sinon.stub().yieldsAsync(new Error('create channel'));
 
-    var connect = amqplibConnect(stub);
     connect('amqp://guest:guest@localhost:5672', function (err) {
       assert.instanceOf(err, Error);
       done();
@@ -60,8 +62,6 @@ describe('amqplib support', function () {
         queue: 'myQueueName'
       };
 
-      var stub = getStubAmqplib();
-      var connect = amqplibConnect(stub);
       connect('amqp://guest:guest@localhost:5672', function (err, channel) {
         assert.isNull(err);
         channel.bind(input, function (err) {
@@ -78,7 +78,6 @@ describe('amqplib support', function () {
     it('can receive messages from a queue', function (done) {
       var handler = sinon.spy();
       var callback = function () { return; };
-      var stub = getStubAmqplib();
 
       stub.channel.consume = function (q, h, cb) {
         assert.equal(q, 'myQueue');
@@ -87,7 +86,6 @@ describe('amqplib support', function () {
         done();
       };
 
-      var connect = amqplibConnect(stub);
       connect('amqp://guest:guest@localhost:5672', function (err, channel) {
         assert.isNull(err);
         channel.consume('myQueue', handler, callback);
@@ -96,10 +94,7 @@ describe('amqplib support', function () {
   });
 
   describe('produce', function () {
-    var channel, stub;
     beforeEach(function (done) {
-      stub = getStubAmqplib();
-      var connect = amqplibConnect(stub);
       connect('amqp://guest:guest@localhost:5672', function (err, chan) {
         channel = chan;
         done(err);
